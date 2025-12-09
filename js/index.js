c318a09b454694a2e8d61b617179cd2f87a23241
João Pedro Telles Fava <88334985+jaypeter19@users.noreply.github.com>
2025-12-09 15:55:49 -0300

    js code for email js logic

---
+++ js/index.js
@@ -0,0 +1,56 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    emailjs.init("c4hFRHeVWjzZIB0wy");
+
+    const form = document.getElementById('contactForm');
+
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+
+        if (!form.checkValidity()) {
+            form.classList.add('was-validated');
+            return;
+        }
+
+        const formData = {
+            name: form.name.value,
+            email: form.email.value,
+            phone: form.phone.value,
+            message: form.message.value
+        };
+
+        emailjs.send('service_a1xsijq', 'template_hinmpzt', formData)
+            .then((response) => {
+                console.log('E-mail enviado com sucesso!', response.status, response.text);
+                form.reset();
+                form.classList.remove('was-validated');
+
+
+                // Redireciona para whatsapp
+                window.location.href = "https://wa.me/5551989184786";
+            })
+            .catch((error) => {
+                console.error('Erro ao enviar e-mail:', error);
+            });
+    });
+});
